aa51cf9865398667cc70358c853788f506d6ac5b
arthurthomasp21 <92447480+arthurthomasp21@users.noreply.github.com>
2022-01-28 10:15:19 +0100

    début affichage

--- rogue.py
+++ rogue.py
@@ -14,13 +14,15 @@ tick = 10
 running = True
 while running:
     clock.tick(tick)
+
+
+    #SECTION 1 :
+    #lecture des événements :
+
     for event in pg.event.get():
-        
         if event.type == pg.QUIT:
             running = False
-        
         elif event.type == pg.KEYDOWN:
-            
             if event.key == pg.K_UP :
                 
             elif event.key == pg.K_DOWN:
@@ -29,4 +31,13 @@ while running:
                 
             elif event.key == pg.K_RIGHT:
                 
+    #SECTION 2 : affichage
+    screen.fill((0,0,0)) #fond noir
+    for pix in pixels :
+            
+        rect = pg.Rect(x*20, y*20, 20, 20)
+        color = (0,0,0)
+        pg.draw.rect(screen, color, rect)
     
+    pg.display.set_caption(f"Score: {score}") #Affichage du score
+    pg.display.update()
